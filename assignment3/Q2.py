076de46dbd0fed87415ba7e6eb6a3cd7ee313cd0
pieterdeboer <Pieterdeboer@tudelft.nl>
2025-04-18 14:34:44 +0200

    .

--- assignment3/Q2.py
+++ assignment3/Q2.py
@@ -10,7 +10,7 @@ import TudatPropagator as prop
 import ConjunctionUtilities as ConjUtil
 
 from mpl_toolkits.mplot3d import Axes3D 
-from tudatpy import constants
+from tudatpy import constants,astro
 
 from matplotlib.animation import FuncAnimation
 from scipy.optimize import least_squares
@@ -74,7 +74,6 @@ Define propagation data
 propagated_data = {}
 t0 = (datetime(2025, 4, 1, 12, 0, 0) - datetime(2000, 1, 1, 12, 0, 0)).total_seconds()
 t_vec = np.array([t0,t0+24*3600])
-Epoch_start_date = constants.JULIAN_DAY_ON_J2000+t0/constants.JULIAN_DAY
 
 int_params = {}
 int_params['tudat_integrator'] = 'rkf78'
@@ -298,13 +297,6 @@ for tk in time_steps:
 # plot_kalman(meas_dict,np.array(state_list),residuals,covariance,cuttoff=-1)
 manouvre_begin_state  = filter_output[manoeuvre_epoch]['state'] 
 
-'''
-Least square method to construct the post mannouvre orbit
- - get the initial position and intermediate positions from the radar
- - get the initital velocity via lambert targerter
- - LSQ method to find positions and velocities to 'patch' the orbit
-'''
-
 print('Converting Ra,Dec,Slant range to ECI satellite positions....')
 print(f'''--------------------------------------------------------------------------------------------------------------''')
 def compute_r_eci_from_radar(Yk, sensor_params, tk, bodies):
@@ -322,7 +314,6 @@ for idx, tk in enumerate(meas_dict['tk_list']):
 #removing all radar data before the maneouvre
 meas_dict['tk_list'] = meas_dict['tk_list'][idx:]
 meas_dict['Yk_list'] = meas_dict['Yk_list'][idx:]
-
 
 dt_list = np.diff(meas_dict['tk_list'])
 jump_indices = np.where(dt_list > 10)[0]
@@ -351,6 +342,7 @@ lambert_first_arc_initial_state,lambert_first_arc_final_state = prop.lambert_tar
         tk_array[0],
         bodies
     )
+print(tk_array[0])
 lambert_second_arc_initial_state,lambert_second_arc_final_state = prop.lambert_targerter(
         r_eci_array[jump_indices[0]+1],
         r_eci_array[jump_indices[1]],
@@ -430,28 +422,6 @@ LSQ method to estimate position and velocities between the sparse data.
 print(f'''--------------------------------------------------------------------------------------------------------------''')
 print("Performing LSQ to estimate positions and velocities of the new orbit....")
 print(f'''--------------------------------------------------------------------------------------------------------------''')
-def compute_radar_from_r_eci(r_eci, sensor_params, tk_array, bodies):
-    sensor_ecef = sensor_params['sensor_ecef']
-    Yk_list = []
-    for r_eci, tk in zip(r_eci_array, tk_array):
-            # Convert sensor from ECEF to ECI at time tk
-            ecef2eci = bodies.get("Earth").rotation_model.body_fixed_to_inertial_rotation(tk)
-            sensor_eci = np.dot(ecef2eci, sensor_ecef).flatten()
-
-            # Line-of-sight vector
-            rho_vec = r_eci - sensor_eci
-            rg = np.linalg.norm(rho_vec)
-            rho_hat = rho_vec / rg
-
-            # Dec and RA
-            dec = np.arcsin(rho_hat[2])
-            ra = np.arctan2(rho_hat[1], rho_hat[0])
-
-            Yk_list.append([rg, ra, dec])
-    return np.array(Yk_list)
-
-#already filtered from previous analysis, so starts on the first new orbit radar measurement
-Radar_observations = np.array(meas_dict['Yk_list'][:jump_indices[0]])
 
 #states at the beginning of the lambert arc
 observed_state_begin = np.array([
@@ -482,12 +452,12 @@ epoch_e = np.array([
 ])
 
 int_params = {}
-int_params['tudat_integrator'] = 'rk4'
+int_params['tudat_integrator'] = 'rkf78'
 int_params['step'] = 10
-# int_params['max_step'] = 1000.
-# int_params['min_step'] = 0.0001
-# int_params['rtol'] = 1e-12
-# int_params['atol'] = 1e-12
+int_params['max_step'] = 1000.
+int_params['min_step'] = 0.0001
+int_params['rtol'] = 1e-12
+int_params['atol'] = 1e-12
 
 current_obj = rso_dict[91000]
 rso_params = {
@@ -503,37 +473,23 @@ rso_params = {
 
 #propagate the states
 def propagate_state(initial_state, epoch0, target_epochs, rso_params, int_params):
-    """Propagate from initial_state (6D) to each target_epoch and convert states to radar obs."""
+    """Propagate from initial_state (6D) to each target_epoch."""
     results = []
-    ECEF_radar = []
-
     for epoch in target_epochs:
         t_vec = [epoch0, epoch]
         full_state_history = prop.propagate_orbit(initial_state, t_vec, rso_params, int_params)
-
-        final_state = full_state_history[1][-1, :]
+        final_state = full_state_history[1][-1,:]
+        state_positions = full_state_history[1][:,:3]
         results.append(final_state)
-
-        radar_conversion = compute_radar_from_r_eci(
-            full_state_history[1][:, :3], sensor_params, full_state_history[0], bodies
-        )
-        ECEF_radar.append(radar_conversion)
-
-    return np.array(results).flatten(), np.vstack(ECEF_radar)
-
+    return np.array(results).flatten()
 
 #cost function for the LSQ
 def cost_function(state_guess):
     init_state = np.array(state_guess)
-    predicted_state_end, predicted_radar = propagate_state(init_state, epoch_b[0], epoch_e, rso_params, int_params)
-    
-    Radar_obs_flat = np.squeeze(Radar_observations) 
-
-    # state_residual = predicted_state_end - observed_state_end.flatten()
-    radar_residual = (predicted_radar[-1] - Radar_obs_flat).flatten()
-
-    return np.concatenate([ radar_residual])
-
+    predicted = propagate_state(init_state, epoch_b[0], epoch_e, rso_params, int_params)
+    residual = predicted - observed_state_end.flatten()
+    # weight = np.array([1e-3, 1e-3, 1e-3, 1, 1, 1])  # Adjust as needed
+    return residual #* weight
 
 initial_guess = lambert_first_arc_initial_state.flatten()
 bounds = (
@@ -562,6 +518,7 @@ print("Position norm:", np.linalg.norm(best_position), 'm')
 print("Velocity norm:", np.linalg.norm(best_velocity), 'm/s')
 # print("covariance", cov)
 # print("standard deviation", np.array([np.sqrt(np.diag(cov))])  )
+
 '''
 Computing dV by finding the needed velocity to go to the last radar measurement via the lambert targetter,
 annd substract initial vs needed velocity.
@@ -575,8 +532,13 @@ initial_state,final_state = prop.lambert_targerter(
 Velocity_needed = np.array(initial_state[3:]).flatten()
 Velocity_currennt = np.array(manouvre_begin_state[3:]).flatten()
 Change = Velocity_needed-Velocity_currennt
+RSW_matrix = astro.frame_conversion.inertial_to_rsw_rotation_matrix(initial_state)
+dv_RSW = np.dot(RSW_matrix.T, Change)
+
 print(f'dV vector {Change} [m/s]')
-print(f'dV nonrm {np.linalg.norm(Change)} [m/s]')
+print(f'dV in RNT vector {dv_RSW} [m/s]')
+print(f'dV norm {np.linalg.norm(Change)} [m/s]')
+print(f'dV in RNT norm {np.linalg.norm(dv_RSW)} [m/s]')
 '''
 Propagate object 91000 with TUDAT
 save the data in propagated_data under the post mannouvre name
--- assignment3/dump.py
+++ assignment3/dump.py
@@ -94,3 +94,142 @@ print("Position norm:", np.linalg.norm(best_position), 'm')
 print("Velocity norm:", np.linalg.norm(best_velocity), 'm/s')
 # print("covariance", cov)
 # print("standard deviation", np.array([np.sqrt(np.diag(cov))])  )
+
+
+
+def compute_radar_from_r_eci(r_eci, sensor_params, tk_array, bodies):
+    sensor_ecef = sensor_params['sensor_ecef']
+    Yk_list = []
+    for r_eci, tk in zip(r_eci_array, tk_array):
+            # Convert sensor from ECEF to ECI at time tk
+            ecef2eci = bodies.get("Earth").rotation_model.body_fixed_to_inertial_rotation(tk)
+            sensor_eci = np.dot(ecef2eci, sensor_ecef).flatten()
+
+            # Line-of-sight vector
+            rho_vec = r_eci - sensor_eci
+            rg = np.linalg.norm(rho_vec)
+            rho_hat = rho_vec / rg
+
+            # Dec and RA
+            dec = np.arcsin(rho_hat[2])
+            ra = np.arctan2(rho_hat[1], rho_hat[0])
+
+            Yk_list.append([rg, ra, dec])
+    return np.array(Yk_list)
+
+#already filtered from previous analysis, so starts on the first new orbit radar measurement
+Radar_observations = np.array(meas_dict['Yk_list'][:jump_indices[0]])
+
+#states at the beginning of the lambert arc
+observed_state_begin = np.array([
+    lambert_first_arc_initial_state,
+    # lambert_second_arc_initial_state,
+    # lambert_third_arc_initial_state
+])
+
+#states at the end of the lambert arc
+observed_state_end = np.array([
+    lambert_first_arc_final_state,
+    # lambert_second_arc_final_state,
+    # lambert_third_arc_final_state
+])
+
+#Epochs at the beginning of the lambert arc
+epoch_b = np.array([
+    tk_array[0], 
+    # tk_array[jump_indices[0]+1],
+    # tk_array[jump_indices[1]+1]
+])
+
+#Epochs at the end of the lambert arc
+epoch_e = np.array([
+    tk_array[jump_indices[0]], 
+    # tk_array[jump_indices[1]],
+    # tk_array[-1]
+])
+
+int_params = {}
+int_params['tudat_integrator'] = 'rk4'
+int_params['step'] = 10
+int_params['max_step'] = 1000.
+int_params['min_step'] = 0.0001
+int_params['rtol'] = 1e-12
+int_params['atol'] = 1e-12
+
+current_obj = rso_dict[91000]
+rso_params = {
+    'mass': current_obj['mass'],
+    'area': current_obj['area'],
+    'Cd': current_obj['Cd'],
+    'Cr': current_obj['Cr'],
+    'sph_deg': 8,
+    'sph_ord': 8,
+    'central_bodies': ['Earth'],
+    'bodies_to_create': ['Earth', 'Sun', 'Moon']
+}
+
+#propagate the states
+def propagate_state(initial_state, epoch0, target_epochs, rso_params, int_params):
+    """Propagate from initial_state (6D) to each target_epoch and convert states to radar obs."""
+    results = []
+    ECEF_radar = []
+
+    for epoch in target_epochs:
+        t_vec = [epoch0, epoch]
+        full_state_history = prop.propagate_orbit(initial_state, t_vec, rso_params, int_params)
+
+        final_state = full_state_history[1][-1, :]
+        results.append(final_state)
+
+        radar_conversion = compute_radar_from_r_eci(
+            full_state_history[1][:, :3], sensor_params, full_state_history[0], bodies
+        )
+        ECEF_radar.append(radar_conversion)
+
+    return np.array(results).flatten(), np.vstack(ECEF_radar)
+
+
+#cost function for the LSQ
+def cost_function(state_guess):
+    init_state = np.array(state_guess)
+    predicted_state_end, predicted_radar = propagate_state(init_state, epoch_b[0], epoch_e, rso_params, int_params)
+    
+    Radar_obs_flat = np.squeeze(Radar_observations) 
+
+    state_residual = predicted_state_end - observed_state_end.flatten()
+    radar_residual = (predicted_radar[-1] - Radar_obs_flat).flatten()
+
+    return np.concatenate([state_residual, radar_residual])
+
+
+initial_guess = manouvre_begin_state.flatten()
+bounds = (
+    [-1e7, -1e7, -1e7, -15e3, -15e3, -15e3],
+    [ 1e7,  1e7,  1e7,  15e3,  15e3,  15e3]
+)
+
+result = least_squares(
+    cost_function,
+    initial_guess,
+    method='lm', #trf , lm (no bounds)
+    # max_nfev=50,   #limits the number of iterations
+    verbose=2,      #Progress of the LSQ 
+    # bounds = bounds
+)
+
+# Final result
+best_state = result.x
+best_position = best_state[:3]
+best_velocity = best_state[3:]
+# J = result.jac
+# cov = np.linalg.inv(J.T @ J)
+print("Best-fit position:", best_position, 'm')
+print("Best-fit velocity:", best_velocity, 'm/s')
+print("Position norm:", np.linalg.norm(best_position), 'm')
+print("Velocity norm:", np.linalg.norm(best_velocity), 'm/s')
+# print("covariance", cov)
+# print("standard deviation", np.array([np.sqrt(np.diag(cov))])  )
+'''
+Computing dV by finding the needed velocity to go to the last radar measurement via the lambert targetter,
+annd substract initial vs needed velocity.
+'''
